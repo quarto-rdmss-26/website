389f8f735f311b97d5ed5bb8e87b315428eee42e
larnsce <lars@lse.de>
2026-05-11 16:00:11 +0200

    update links

--- data/get_course_data.R
+++ data/get_course_data.R
@@ -15,7 +15,7 @@ library(stringr)
 
 # course-schedule
 
-link_lesson_plan <- "https://docs.google.com/spreadsheets/d/1OLdrg5ZN6-V91PMcK4AuUfH9vLrNkxESBh7aVPnr9Ig/edit?gid=0#gid=0"
+link_lesson_plan <- "https://docs.google.com/spreadsheets/d/1b2osej0p-3rg12_uPwjlWC5mAXmbVX_jnipqdHnY7CQ/edit?gid=0#gid=0"
 
 googlesheets4::read_sheet(link_lesson_plan) |>
   mutate(title = case_when(
@@ -27,4 +27,4 @@ googlesheets4::read_sheet(link_lesson_plan) |>
   mutate(end_time = as.character(end_time)) |>
   mutate(end_time = str_extract(end_time, "\\b\\d{2}:\\d{2}\\b")) |>
   mutate(time = paste(start_time, end_time, sep = " - ")) |>
-  write_csv(here::here("data/tbl-01-quarto-rdmss-25-course-schedule.csv"))
+  write_csv(here::here("data/tbl-01-quarto-rdmss-26-course-schedule.csv"))
